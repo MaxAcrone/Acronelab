89ecdf7a29dd6fbaeb5d7b016ef6a387d03fab98
MaxAcrone <beedbeedmax@gmail.com>
2025-09-11 01:12:15 +0500

    fix: add source files, remove .next from repo

--- packages/frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components\\\\ClientCursorEffect.tsx -> ./CursorEffect\":{\"id\":\"components\\\\ClientCursorEffect.tsx -> ./CursorEffect\",\"files\":[\"static/chunks/_app-pages-browser_src_components_CursorEffect_tsx.js\"]}}"
